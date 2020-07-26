9af021d6de0275c16b5abd7d8123784ad3dd9062
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2020-07-26 14:12:12 -0500

    feat: Added .spec.ts file for server

---
+++ src/server.spec.ts
@@ -0,0 +1,22 @@
+import {Server} from "./server";
+import express from "express";
+import request from "supertest";
+
+describe('server.ts', () => {
+    const server: express.Application = new Server().start();
+
+    it('GET / request should return a response code 200', async () => {
+        const response = await request(server).get('/');
+        expect(response.status).toEqual(200);
+    });
+
+    it('GET /api request should return a response code 200', async () => {
+        const response = await request(server).get('/api');
+        expect(response.status).toEqual(200);
+    });
+
+    it('GET /api/v1 request should return a response code 200', async () => {
+        const response = await request(server).get('/api/v1');
+        expect(response.status).toEqual(200);
+    });
+});
