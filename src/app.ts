647903c384fbf2c4becfc627affb9569c63b2259
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2020-08-02 15:15:50 -0500

    fix(app.ts): Solved bad http implementation

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,21 @@
 import {Server} from "./server";
-import http from "http";
+import http from "http2";
 import express from "express";
 
 export class App extends Server {
 
-    private _http: http.Server;
+    private _http: http.Http2Server;
+    private readonly _application: express.Application;
 
     constructor() {
         super();
-        this._http = new http.Server();
+        this._application = this.start();
+        this._http = http.createServer(this._application);
         this.init();
     }
 
     private init() {
-        const server: express.Application = this.start();
-        const port = server.get('PORT');
+        const port = this._application.get('PORT');
         this._http.listen(port, () => {
             console.log(`Server running on port: ${port}...`);
         });
